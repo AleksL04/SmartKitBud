f79de554e4bb13a24261b60f5d2b0bde79e145d3
Aleks <sasha.likhoshva@gmail.com>
2025-06-25 14:36:45 -0400

    c

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 OCR_API_KEY: str = os.environ.get("OCR_API_KEY")
 GEMINI_API_KEY: str = os.environ.get("GEMINI_API_KEY")
-SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+SUPABASE_URL: str = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY: str = os.environ.get("SUPABASE_KEY")
 UPLOAD_DIR: str = "uploads"
--- backend/format_json.py
+++ backend/format_json.py
@@ -1,3 +1,5 @@
+import json
+
 from google import genai
 from google.genai import types
 
@@ -6,16 +8,18 @@ Please review the item names in the provided JSON array for any spelling errors 
 Additionally, convert all item names to lowercase. 
 Ensure that numerical values and their associated units are accurately represented (e.g., '44DGR' should be interpreted as '440gr' if 'D' represents '0' in this context). 
 Maintain the exact JSON structure. 
+Respond only with a JSON array. Do not include any additional text or markdown quotes. If no items are found, return an empty array [].
 """
 
-def format_json(extracted_text: str, api_key: str):
+def format_json_to_lowercase(raw_json_string: str, api_key: str):
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=[_GEMINI_INSTRUCTION,extracted_text],
+        model="gemini-2.5-flash", contents=[raw_json_string, _GEMINI_INSTRUCTION],
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=0), # Disables thinking
             temperature=0
         ),
     )
-    return response.text
+    python_dict = json.loads(response.text)
+    return python_dict
