71dd905efe9039dead0b6a13a342e6dfb4d429c4
Aleks <sasha.likhoshva@gmail.com>
2025-07-05 16:17:32 -0400

    c

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{ hostname: "upload.wikimedia.org" }, { hostname: "pngimg.com" }],
+  },
 };
 
 export default nextConfig;
